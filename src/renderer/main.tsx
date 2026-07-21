4b453521049e2015cfa6d2b7d821359cae55f53c
santosjohnmarkb-CMB <santos.johnmarkb@gmail.com>
2026-07-21 11:44:15 +0800

    Update HashRouter configuration to support future features in main.tsx

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -17,7 +17,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter>
+    <HashRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <App />
     </HashRouter>
   </React.StrictMode>,
